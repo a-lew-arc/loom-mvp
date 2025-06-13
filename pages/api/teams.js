22eed702ae2c1fe05b4875e463db12c343e52f7c
a-lew-arc <albert.lewis@archetype.co>
2025-06-13 16:06:30 -0400

    Update teams.js

--- pages/api/teams.js
+++ pages/api/teams.js
@@ -3,7 +3,7 @@ import { getSheetData } from '../../lib/sheets';
 
 export default async function handler(req, res) {
   const sheetId = '1FvuWgmK4H3qxmgDEtoXbHL1JBYdc4WiivjM2Mj_UpfA';
-  const sheetRange = 'Teams!A:Z'; // exact tab name + column range
+  const sheetRange = 'Team!A:Z'; // exact tab name + column range
 
   try {
     const rows = await getSheetData(sheetId, sheetRange);
